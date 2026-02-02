63d36f029d7983d7cfbe123d710a7e76b80e26ac
rpariona81 <ronald.pariona.isidro@gmail.com>
2026-02-02 18:07:46 -0500

    comment controller de pruebas

--- webapp/application/config/routes.php
+++ webapp/application/config/routes.php
@@ -49,15 +49,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+//$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'authcontroller';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 //$route['testlogin:(:any)/(:any)'] = 'welcome/testlogin/$1/$2';
-$route['testlogin'] = 'welcome/testlogin';
+/* $route['testlogin'] = 'welcome/testlogin';
 $route['testlogin1'] = 'welcome/testlogin1';
 $route['testlogin2'] = 'welcome/testlogin2';
-$route['testlogin3'] = 'welcome/testlogin3';
+$route['testlogin3'] = 'welcome/testlogin3'; */
 
 
 $route['login'] = 'authcontroller/index';
